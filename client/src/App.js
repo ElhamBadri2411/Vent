7478c35c32ee8ac6120d5490997f1921cbf5037e
ElhamBadri2411 <elhambadri24@gmail.com>
2021-09-18 16:04:55 -0400

    backend v1 done

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Form from './Form/Form.jsx'
+import  { getMessage } from './api/api.js'
 
 function App() {
   const [currentMessage, setCurrentMessage] = useState(null);
