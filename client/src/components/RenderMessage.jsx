c5a759dc4fadc0fb7f6e8c4ceac3752c5884974c
hadialhakeem <hadihakeem2@gmail.com>
2021-09-19 00:07:53 -0400

    stuff

--- client/src/components/RenderMessage.jsx
+++ client/src/components/RenderMessage.jsx
@@ -6,10 +6,10 @@ function RenderMessage(props) {
 
     return (
         <div>
-            <Card borderRadius='15px' elevation={12} style={{width: "75%", padding: "2em", textAlign: 'left'}}>
-                <Typography variant="h4">{message.title}</Typography>
+            <Card borderRadius='15px' elevation={12} style={{width: "75%", padding: "2em", textAlign: 'left', backgroundColor: '#404040'}}>
+                <Typography variant="h4" style={{color: '#fff'}}>{message.title}</Typography>
                 <br />
-                <Typography variant="body1" style={{whiteSpace: "pre-line", textAlign: 'left'}}>{message.message}</Typography>
+                <Typography variant="body1" style={{whiteSpace: "pre-line", color: '#fff'}}>{message.message}</Typography>
             </Card>
         </div>
     )
