04611a351f2e045f889168911900289f38120a6c
hadialhakeem <hadihakeem2@gmail.com>
2021-09-18 23:19:51 -0400

    fixed stuff

--- client/src/components/RenderMessage.jsx
+++ client/src/components/RenderMessage.jsx
@@ -9,7 +9,7 @@ function RenderMessage(props) {
             <Card borderRadius='15px' elevation={12} style={{width: "75%", padding: "2em"}}>
                 <Typography variant="h4">{message.title}</Typography>
                 <br />
-                <Typography variant="body1">{message.message}</Typography>
+                <Typography variant="body1" style={{whiteSpace: "pre-line"}}>{message.message}</Typography>
             </Card>
         </div>
     )
