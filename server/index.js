a763c49ed04df2949899e450623eda40e614edf5
hadialhakeem <hadihakeem2@gmail.com>
2021-09-19 01:48:19 -0400

    stuff

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use("/message", messageRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://VentHacktheNorth:VentHacktheNorth12345@cluster0.cvye3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = process.env.DB_URL
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {
